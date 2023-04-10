1b1c78ae7972b5011623e6690fcdcb5644c845fe
Vasil1001 <vaskodzhakov@gmail.com>
2023-04-10 14:04:09 +0100

    swc
    swc

---
+++ .history/vite.config_20230410135941.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import swc from '@swc/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [swc()],
+})
