efc5f669c76c44549f9b4225c665cef548388736
Vasil <vaskodzhakov@gmail.com>
2023-05-11 02:04:16 +0100

    Skill badges added to home page projects
    Skill badges added to home page projects

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -26,18 +26,34 @@ export default function Projects() {
           <div className="mb-3.5 grid grid-cols-4 flex-wrap  gap-3.5 md:grid-cols-3 lg:grid-cols-4">
             <div className="projMainPageCard col-span-full text-center dark:bg-[#edeeef] md:col-span-2">
               <div className="flex justify-between">
-                <h1 className="px-2 py-1 text-left font-noto text-3xl font-semibold text-[#202124] dark:text-[#383b41] md:mb-3 md:text-4xl">
+                <h1 className="px-2 pt-1 text-left font-noto text-3xl font-semibold text-[#202124] dark:text-[#383b41] md:mb-3 md:text-4xl">
                   Nutrition Tracker
                   <hr className="mt-2 " />
                 </h1>
+
                 <HiOutlineExternalLink className="car cursor-pointer hover:text-[#207c86]" size={'1.4rem'} />
               </div>
+
               <p className="homeProjectDescription">
                 {/* A personal React project built to help me calculate my daily nutrient intake. It's main purpose is to assist me in my gym journey and
                 help me eat more, while learning about proper nutrition, protein, eating healhty, tracking my progress and documenting my research. */}
                 A website that accompanies my gym journey by helping calculate my daily nutrient needs motivates me to hit my set targets.
               </p>
-              <img src={nutrition} className="card2 rounded-xl" alt="home" />
+              <div className="my-1 flex gap-1 px-1">
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="react.js"
+                  src="https://img.shields.io/badge/-React.js-27272a?logo=react&style=flat"
+                ></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="css"
+                  src="https://img.shields.io/badge/-CSS3-27272a?logo=css3&amp;style=flat&logoColor=white"
+                ></img>
+              </div>
+              <img src={nutrition} className="projCardScaleUpHover rounded-xl" alt="home" />
             </div>
             <div className="cardQuiz col-span-2 flex flex-col items-center justify-center rounded-2xl border-2 border-[#ebebeb] bg-[#f1f0ee] text-center dark:hover:border-[#000] md:col-span-1 lg:col-span-1">
               <div className="card2 flex min-h-full min-w-full flex-wrap justify-center  overflow-hidden rounded-xl p-1">
@@ -53,7 +69,7 @@ export default function Projects() {
 
             <div className="projMainPageCard col-span-4 text-center dark:bg-[#edeeef] md:col-span-2">
               <div className="flex justify-between">
-                <h1 className="px-2 py-1 text-left font-noto text-3xl font-semibold text-[#202124] dark:text-[#383b41] md:mb-3 md:text-4xl">
+                <h1 className="px-2 pt-1 text-left font-noto text-3xl font-semibold text-[#202124] dark:text-[#383b41] md:mb-3 md:text-4xl">
                   Github Finder
                   <hr className="mt-2 " />
                 </h1>
@@ -63,7 +79,29 @@ export default function Projects() {
               <p className="homeProjectDescription">
                 A project that uses GitHub API to search for profiles and display information about them like latest repositories.
               </p>
-              <img src={githubFinder} className="card2 rounded-xl" alt="home" />
+              <div className="my-1 mb-1.5 flex flex-wrap gap-1 px-1">
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="react.js"
+                  src="https://img.shields.io/badge/-React.js-27272a?logo=react&style=flat"
+                ></img>
+                <img class="select-none" draggable="false" alt="Vite" src="https://img.shields.io/badge/-Vite-27272a?logo=vite&style=flat"></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="TailwindCSS"
+                  src="https://img.shields.io/badge/-TailwindCSS-27272a?logo=TailwindCSS&style=flat"
+                ></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="daisyui"
+                  src="https://img.shields.io/badge/-DaisyUI-27272a?logo=daisyui&style=flat"
+                ></img>
+                <img class="select-none" draggable="false" alt="context" src="https://img.shields.io/badge/-ContextAPI-27272a?logo=&style=flat"></img>
+              </div>
+              <img src={githubFinder} className="projCardScaleUpHover rounded-xl" alt="home" />
             </div>
 
             <div className="projMainPageCard col-span-full text-center dark:bg-[#edeeef] lg:col-span-2">
@@ -72,12 +110,35 @@ export default function Projects() {
                   Rently
                   <hr className="mt-2 " />
                 </h1>
+
                 <HiOutlineExternalLink className="car cursor-pointer hover:text-[#207c86]" size={'1.4rem'} />
               </div>
+
               <p className="homeProjectDescription">
                 A property rental project that allows users to rent their property or get in contact with a landlord and find the right home.
               </p>
-              <img src={homeRently} className=" card2 overflow-hidden rounded-xl" alt="home" />
+              <div className="my-1 mb-1.5 flex flex-wrap gap-1 px-1">
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="react.js"
+                  src="https://img.shields.io/badge/-React.js-27272a?logo=react&style=flat"
+                ></img>
+                <img class="select-none" draggable="false" alt="Vite" src="https://img.shields.io/badge/-Vite-27272a?logo=vite&style=flat"></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="TailwindCSS"
+                  src="https://img.shields.io/badge/-TailwindCSS-27272a?logo=TailwindCSS&style=flat"
+                ></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="daisyui"
+                  src="https://img.shields.io/badge/-DaisyUI-27272a?logo=daisyui&style=flat"
+                ></img>
+              </div>
+              <img src={homeRently} className=" projCardScaleUpHover overflow-hidden rounded-xl" alt="home" />
             </div>
 
             <div className="projMainPageCard col-span-full text-center dark:bg-[#edeeef] lg:col-span-2">
@@ -88,7 +149,46 @@ export default function Projects() {
                 </h1>
                 <HiOutlineExternalLink className="car cursor-pointer hover:text-[#207c86]" size={'1.4rem'} />
               </div>
-              <img src={ticketDesk} className="card2 rounded-xl" alt="home" />
+              <div className="my-1 mb-1.5 flex flex-wrap gap-1 px-1">
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="mongodb"
+                  src="https://img.shields.io/badge/-MongoDB-27272a?logo=mongodb&style=flat"
+                ></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="express.js"
+                  src="https://img.shields.io/badge/-Express.js-27272a?logo=express&style=flat"
+                ></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="react.js"
+                  src="https://img.shields.io/badge/-React.js-27272a?logo=react&style=flat"
+                ></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="node"
+                  src="https://img.shields.io/badge/-Node.js-27272a?logo=Node.js&style=flat"
+                ></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="TailwindCSS"
+                  src="https://img.shields.io/badge/-TailwindCSS-27272a?logo=TailwindCSS&style=flat"
+                ></img>
+                <img class="select-none" draggable="false" alt="Vite" src="https://img.shields.io/badge/-Vite-27272a?logo=vite&style=flat"></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="daisyui"
+                  src="https://img.shields.io/badge/-DaisyUI-27272a?logo=daisyui&style=flat"
+                ></img>
+              </div>
+              <img src={ticketDesk} className="projCardScaleUpHover rounded-xl" alt="home" />
             </div>
 
             <div className="projMainPageCard col-span-full text-center dark:bg-[#edeeef] lg:col-span-2">
@@ -99,7 +199,23 @@ export default function Projects() {
                 </h1>
                 <HiOutlineExternalLink className="car cursor-pointer hover:text-[#207c86]" size={'1.4rem'} />
               </div>
-              <img src={portfoliod} className="card2 rounded-xl" alt="home" />
+
+              <div className="my-1 mb-1.5 flex flex-wrap gap-1 px-1">
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="react.js"
+                  src="https://img.shields.io/badge/-React.js-27272a?logo=react&style=flat"
+                ></img>
+                <img class="select-none" draggable="false" alt="Vite" src="https://img.shields.io/badge/-Vite-27272a?logo=vite&style=flat"></img>
+                <img
+                  class="select-none"
+                  draggable="false"
+                  alt="TailwindCSS"
+                  src="https://img.shields.io/badge/-TailwindCSS-27272a?logo=TailwindCSS&style=flat"
+                ></img>
+              </div>
+              <img src={portfoliod} className="projCardScaleUpHover rounded-xl" alt="home" />
             </div>
 
             <div className="col-span-full flex max-h-[500px] gap-3.5 overflow-hidden transition-all hover:rounded-tl-[27px] hover:transition-all">
@@ -124,7 +240,33 @@ export default function Projects() {
                   Fullstack project built after university to expand on Java from my classes. Frontend was written in thymeleaf, and it is what made
                   me explore frontend frameworks and learning React!
                 </p>
-                <img src={dashboardSpring} className="card2 overflow-hidden rounded-xl  object-contain" alt="home" />
+                <div className="my-1 mb-1 flex flex-wrap gap-1 px-1">
+                  <img
+                    class="select-none"
+                    draggable="false"
+                    alt="java"
+                    src="https://img.shields.io/badge/-Java-27272a?logo=openjdk&amp;style=flat&logoColor=red"
+                  ></img>
+                  <img
+                    class="select-none"
+                    draggable="false"
+                    alt="spring"
+                    src="https://img.shields.io/badge/-Spring Boot-27272a?logo=spring&amp;style=flat&logoColor=green"
+                  ></img>
+                  <img
+                    class="select-none"
+                    draggable="false"
+                    alt="html"
+                    src="https://img.shields.io/badge/-HTML-27272a?logo=html5&amp;style=flat&logoColor=orange"
+                  ></img>
+                  <img
+                    class="select-none"
+                    draggable="false"
+                    alt="css"
+                    src="https://img.shields.io/badge/-CSS3-27272a?logo=css3&amp;style=flat&logoColor=white"
+                  ></img>
+                </div>
+                <img src={dashboardSpring} className="projCardScaleUpHover overflow-hidden rounded-xl  object-contain" alt="home" />
 
                 {/* <p className="text-md text-left font-roboto text-gray-600 dark:text-[#383b41] md:text-lg">
                 React app to search for GitHub profiles and display available information about user, description and latest projects.
@@ -147,11 +289,43 @@ export default function Projects() {
                   </h1>
                   <HiOutlineExternalLink className="car cursor-pointer hover:text-[#207c86]" size={'1.4rem'} />
                 </div>
-                <p className="text-md px-2 pb-3 text-justify font-roboto text-gray-600 dark:text-[#383b41] md:text-lg">
+                <p className="homeProjectDescription">
                   A NextJS fullstack project that allows users to generate an image from typing a prompt and describing the image they want. The
                   project is in progress, the image below is a concept of what it should look like.
                 </p>
-                <img src={ai_concept} className="card2 rounded-xl" alt="home" />
+                <div className="my-1 mb-1.5 flex flex-wrap gap-1 px-1">
+                  <img
+                    className="select-none"
+                    draggable="false"
+                    alt="next.js"
+                    src="https://img.shields.io/badge/-NEXT.JS-27272a?logo=next.js&amp;style=flat"
+                  ></img>
+                  <img
+                    className="select-none"
+                    draggable="false"
+                    alt="typescript"
+                    src="https://img.shields.io/badge/-TypeScript-27272a?logo=typescript&amp;style=flat"
+                  ></img>
+                  <img
+                    class="select-none"
+                    draggable="false"
+                    alt="TailwindCSS"
+                    src="https://img.shields.io/badge/-TailwindCSS-27272a?logo=TailwindCSS&style=flat"
+                  ></img>
+                  <img
+                    class="select-none"
+                    draggable="false"
+                    alt="PostgreSQL"
+                    src="https://img.shields.io/badge/-PostgreSQL-27272a?logo=PostgreSQL&style=flat&logoColor=white"
+                  ></img>
+                  <img
+                    class="select-none"
+                    draggable="false"
+                    alt="Prisma"
+                    src="https://img.shields.io/badge/-Prisma-27272a?logo=Prisma&style=flat"
+                  ></img>
+                </div>
+                <img src={ai_concept} className="projCardScaleUpHover rounded-xl" alt="home" />
               </div>
 
               <div className="projMainPageCard grow-1 float-right w-full flex-wrap text-center dark:bg-[#edeeef] md:w-1/2">
@@ -165,7 +339,21 @@ export default function Projects() {
                 <p className="text-md px-2 pb-3 text-justify font-roboto text-gray-600 dark:text-[#383b41] md:text-lg">
                   A simple React application created to practice with Tailwind layouts using the Pokedex API.
                 </p>
-                <img src={pokeView} className="card2 rounded-xl" alt="home" />
+                <div className="my-1 mb-1.5 flex flex-wrap gap-1 px-1">
+                  <img
+                    class="select-none"
+                    draggable="false"
+                    alt="react.js"
+                    src="https://img.shields.io/badge/-React-27272a?logo=react&style=flat"
+                  ></img>
+                  <img
+                    class="select-none"
+                    draggable="false"
+                    alt="TailwindCSS"
+                    src="https://img.shields.io/badge/-TailwindCSS-27272a?logo=TailwindCSS&style=flat"
+                  ></img>
+                </div>
+                <img src={pokeView} className="projCardScaleUpHover rounded-xl" alt="home" />
               </div>
             </div>
           </div>
