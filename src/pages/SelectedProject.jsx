8a21c2d0625214686b619ff3060f2a34c88ca326
Vasil <vaskodzhakov@gmail.com>
2023-05-19 18:11:06 +0100

    Deployment fix
    Deployment fix

--- src/pages/SelectedProject.jsx
+++ src/pages/SelectedProject.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function SelectedProject() {
   return (
-    <div>SelectedProject</div>
+    <div>SelectedProject
+      <div className="grid grid-cols-4">
+        <div className="grid-span-1">Tech used in Vertical stack</div>
+        <div className="grid-span-3"> Description</div>
+      </div>
+
+    </div>
   )
 }
