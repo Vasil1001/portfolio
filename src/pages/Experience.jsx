575056078c41c5988d6da15ab52dc8f528236091
Vasil <vaskodzhakov@gmail.com>
2023-05-23 01:29:02 +0100

    final change experience cards
    final change experience cards

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -4,7 +4,13 @@ import Timeline from '../components/Timeline'
 export default function Experience() {
   return (
     <div className="mb-0 ">
+       <div className="col-span-4 mb-3.5  row-span-2 rounded-xl border border-[#e5e4e4] bg-[#f5f4f3] p-6 lg:col-span-4">
+          <div className="flex justify-between">
+            <h1 className="px-2  text-left font-supreme text-3xl font-semibold text-[#2f3136] dark:text-[#383b41] md:text-4xl">Work Experience</h1>
+        </div>
+      </div>
       <div className="mb-3.5 flex flex-col items-center">
+        
         <div className="grid w-full grid-cols-1 justify-center gap-3.5 px-3.5 sm:grid-cols-1 sm:px-0 md:grid-cols-2 lg:grid-cols-3">
           <a className="relative flex h-[325px] flex-col justify-end rounded-2xl border-8 border-[#f1f0ee] bg-[#f1f0ee] text-[#242527] transition-all delay-150  duration-300 ease-in-out hover:font-bold hover:text-[#ffffff] hover:transition-all hover:duration-300 dark:border-[#edeeef] dark:bg-[#edeeef]">
             <div className="absolute inset-0 h-[14.5rem] w-full  overflow-hidden rounded-xl  bg-[#404045] transition-all delay-150 duration-300 ease-in-out hover:h-[308px] hover:bg-[#252529] hover:transition-all hover:duration-300">
@@ -70,8 +76,8 @@ export default function Experience() {
       </div>
 
       <div className="grid grid-cols-4 flex-wrap gap-3.5 px-3.5 sm:px-0">
-        <div className="car col-span-4 rounded-2xl border border-[#e3e4e4] bg-[#f1f0ee] p-7 text-center shadow-md dark:bg-[#edeeef]">
-          <h1 className="mb-1 text-left  font-noto text-3xl font-semibold text-[#202124] dark:text-[#383b41] md:text-4xl ">Experience Timeline</h1>
+        <div className="car col-span-4 rounded-2xl border border-[#e3e4e4] bg-[#f1f0ee] px-7 text-center shadow-md dark:bg-[#edeeef]">
+          <h1 className="mt-5 text-left  font-noto text-3xl font-semibold text-[#202124] dark:text-[#383b41] md:text-4xl ">Timeline</h1>
           {/* <Timeline /> */}
 
           <Timeline />
