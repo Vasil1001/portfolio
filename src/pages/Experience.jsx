bf45bfa4f36a718654788d716d42b83ac4f190a9
Vasil <vaskodzhakov@gmail.com>
2023-05-23 01:01:11 +0100

    Added animated zoom in image cards in experience!
    Added animated zoom in image cards in experience!

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -5,31 +5,55 @@ export default function Experience() {
   return (
     <div className="mb-0 ">
       <div className="mb-3.5 flex flex-col items-center">
-        <div className="grid w-full justify-center gap-3.5 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-          <a className="relative flex h-[325px] flex-col justify-end rounded-3xl bg-gray-200 dark:bg-gray-800">
-            <div className="absolute inset-0 h-[13rem] w-full overflow-hidden rounded-3xl bg-gray-500 transition delay-150 duration-300 ease-in-out hover:h-[325px]  hover:bg-indigo-500 hover:transition-all">
-              Image
+        <div className="grid w-full grid-cols-1 justify-center gap-3.5 px-3.5 sm:grid-cols-1 sm:px-0 md:grid-cols-2 lg:grid-cols-3">
+          <a className="relative flex h-[325px] flex-col justify-end rounded-2xl border-8 border-[#edeeef] bg-[#f1f0ee] text-[#242527] transition-all delay-150  duration-300 ease-in-out hover:font-bold hover:text-[#edeeef] hover:transition-all hover:duration-300 dark:border-[#edeeef] dark:bg-[#edeeef]">
+            <div className="absolute inset-0 h-[14.5rem] w-full  overflow-hidden rounded-xl  bg-[#404045] transition-all delay-150 duration-300 ease-in-out hover:h-[308px] hover:bg-[#252529] hover:transition-all hover:duration-300">
+              <img
+                src="https://youteam.io/blog/wp-content/uploads/2021/07/web-developer-min-1.jpeg"
+                className="h-[15rem] w-full object-cover  transition-all delay-150 duration-300 ease-in-out hover:h-[308px] hover:transition-all hover:duration-300"
+                alt="123"
+              />
             </div>
-            <div className="absolute bottom-4 z-0 flex w-full justify-center">Title</div>
-            <div className="flex flex justify-center overflow-hidden whitespace-nowrap rounded-full drop-shadow-md">Desc</div>
+            <div className="hover:text-[#242527] ">
+              <div className="absolute bottom-4 z-0 mb-2 flex w-full p-2 text-2xl  ">Web Developer Intern</div>
+              <div className="flex overflow-hidden whitespace-nowrap rounded-full p-2 px-2.5 font-satoshi text-lg font-normal drop-shadow-md hover:text-[#0b0b0b]">
+                Internship in Seville Spain for Voyager Seville
+              </div>
+            </div>
           </a>
-          <a className="relative flex h-[325px] flex-col justify-end rounded-3xl bg-gray-200 dark:bg-gray-800">
-            <div className="absolute inset-0 h-[13rem] w-full overflow-hidden rounded-3xl bg-gray-500 transition delay-150 duration-300 ease-in-out hover:h-[325px]  hover:bg-indigo-500 hover:transition-all">
-              Image
+          <a className="relative flex h-[325px] flex-col justify-end rounded-2xl border-8 border-[#edeeef] bg-[#f1f0ee] text-[#242527] transition-all delay-150  duration-300 ease-in-out hover:font-bold hover:text-[#edeeef] hover:transition-all hover:duration-300 dark:border-[#edeeef] dark:bg-[#edeeef]">
+            <div className="absolute inset-0 h-[14.5rem] w-full  overflow-hidden rounded-xl  bg-[#404045] transition-all delay-150 duration-300 ease-in-out hover:h-[308px] hover:bg-[#252529] hover:transition-all hover:duration-300">
+              <img
+                src="https://possector.com/wordpress/wp-content/uploads/2013/07/waiter-good-service1.jpg"
+                className="h-[15rem] w-full object-cover  transition-all delay-150 duration-300 ease-in-out hover:h-[308px] hover:transition-all hover:duration-300"
+                alt="123"
+              />
             </div>
-            <div className="absolute bottom-4 z-0 flex w-full justify-center">Title</div>
-            <div className="flex flex justify-center overflow-hidden whitespace-nowrap rounded-full drop-shadow-md">Desc</div>
+            <div className="hover:text-[#242527] ">
+              <div className="absolute bottom-4 z-0 mb-2 flex w-full p-2 text-2xl  ">Waiter</div>
+              <div className="flex overflow-hidden whitespace-nowrap rounded-full p-2 px-2.5 font-satoshi text-lg font-normal drop-shadow-md hover:text-[#0b0b0b]">
+                ASAP Catering London
+              </div>
+            </div>
           </a>
-
-          <a className="relative flex h-[325px] flex-col justify-end rounded-3xl bg-gray-200 dark:bg-gray-800">
-            <div className="absolute inset-0 h-[13rem] w-full overflow-hidden rounded-3xl bg-gray-500 transition delay-150 duration-300 hover:h-[325px] hover:bg-indigo-500  hover:transition-all hover:ease-in-out">
-              Image
+          <a className="relative flex h-[325px] flex-col justify-end rounded-2xl border-8 border-[#edeeef] bg-[#f1f0ee] text-[#242527] transition-all delay-150  duration-300 ease-in-out hover:font-bold hover:text-[#edeeef] hover:transition-all hover:duration-300 dark:border-[#edeeef] dark:bg-[#edeeef]">
+            <div className="absolute inset-0 h-[14.5rem] w-full  overflow-hidden rounded-xl  bg-[#404045] transition-all delay-150 duration-300 ease-in-out hover:h-[308px] hover:bg-[#252529] hover:transition-all hover:duration-300">
+              <img
+                src="https://www.mhwilliams.com/wp-content/uploads/2020/01/11.jpeg"
+                className="h-[15rem] w-full object-cover  transition-all delay-150 duration-300 ease-in-out hover:h-[308px] hover:transition-all hover:duration-300"
+                alt="123"
+              />
             </div>
-            <div className="absolute bottom-4 z-0 flex w-full justify-center">Title</div>
-            <div className="flex flex justify-center overflow-hidden whitespace-nowrap rounded-full drop-shadow-md">Desc</div>
+            <div className="hover:text-[#242527] ">
+              <div className="absolute bottom-4 z-0 mb-2 flex w-full p-2 text-2xl  ">Construction Worker</div>
+              <div className="flex overflow-hidden whitespace-nowrap rounded-full p-2 px-2.5 font-satoshi text-lg font-normal drop-shadow-md hover:text-[#0b0b0b]">
+                RIR Construction Central London
+              </div>
+            </div>
           </a>
         </div>
       </div>
+
       <div className="grid grid-cols-4 flex-wrap gap-3.5 px-3.5 sm:px-0">
         <div className="car col-span-4 rounded-2xl border border-[#e3e4e4] bg-[#f1f0ee] p-7 text-center shadow-md dark:bg-[#edeeef]">
           <h1 className="mb-1 text-left  font-noto text-3xl font-semibold text-[#202124] dark:text-[#383b41] md:text-4xl ">Experience Timeline</h1>
